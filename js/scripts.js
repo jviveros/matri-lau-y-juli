b22b4d74f887b725a0de75e447118f665a41bddc
jviveros <j.viveros72@gmail.com>
2023-11-11 18:15:41 -0500

    Cambios con RSVP

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Boda de Laura & Julián",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jan 27, 2024 15:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jan 28, 2024 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Hacienda San Marcos, Km 2 Nueva Variante Cota, Cundinamarca, Colombia',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "Revisa los detalles en: https://jviveros.github.io/matri-lau-y-juli/"
         }
     });
 
